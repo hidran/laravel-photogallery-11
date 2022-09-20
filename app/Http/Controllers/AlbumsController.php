981eb114e4e4593360b959743b1d7b295dffec65
Hidran Arias <hidran@gmail.com>
2022-09-20 23:30:47 +0200

    Laravel : Eloquent insert,create & save

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -3,15 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Models\Album;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\View\View;
 
 class AlbumsController extends Controller
 {
     /**
      * Display a listing of the resource.
+     *
+     * @param Request $request
+     * @return View
      */
     public function index(Request $request): View
     {
@@ -21,15 +23,15 @@ class AlbumsController extends Controller
         }
         if ($request->has('album_name')) {
             $queryBuilder->where('album_name', 'like',
-                $request->input('album_name').'%');
+                $request->input('album_name') . '%');
         }
         $albums = $queryBuilder->get();
-
         return view('albums.albums', ['albums' => $albums]);
     }
 
     /**
      * Show the form for creating a new resource.
+     *
      */
     public function create(): View
     {
@@ -38,6 +40,10 @@ class AlbumsController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param Request $request
+     *
+     * @return RedirectResponse
      */
     public function store(Request $request): RedirectResponse
     {
@@ -49,7 +55,7 @@ class AlbumsController extends Controller
         $album->album_thumb = '/';
         $res = $album->save();
         //$res =  Album::create($data);
-        $messaggio = $res ? 'Album   '.$data['album_name'].' Created' : 'Album '.$data['album_name'].' was not crerated';
+        $messaggio = $res ? 'Album   ' . $data['album_name'] . ' Created' : 'Album ' . $data['album_name'] . ' was not crerated';
         session()->flash('message', $messaggio);
 
         return redirect()->route('albums.index');
@@ -58,19 +64,21 @@ class AlbumsController extends Controller
     /**
      * Display the specified resource.
      *
+     * @param Album $album
      *
-     * @return void
+     * @return Album $album
      */
-    public function show(Album $album)
+    public function show(Album $album): Album
     {
-        //
+        return $album;
     }
 
     /**
      * Show the form for editing the specified resource.
      *
+     * @param Album $album
      *
-     * @return Response
+     * @return View
      */
     public function edit(Album $album): View
     {
@@ -83,6 +91,10 @@ class AlbumsController extends Controller
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param Request $request
+     * @param Album $album
+     * @return RedirectResponse
      */
     public function update(Request $request, Album $album): RedirectResponse
     {
@@ -90,14 +102,18 @@ class AlbumsController extends Controller
         $album->album_name = $data['album_name'];
         $album->description = $data['description'];
         $res = $album->save();
-        $messaggio = $res ? 'Album   '.$album->album_name.' Updated' : 'Album '.$album->album_name.' was not updated';
+        $messaggio = $res ? 'Album   ' . $album->album_name . ' Updated' : 'Album ' . $album->album_name . ' was not updated';
         session()->flash('message', $messaggio);
-
         return redirect()->route('albums.index');
     }
 
+
     /**
      * Remove the specified resource from storage.
+     *
+     * @param Album $album
+     *
+     * @return int
      */
     public function destroy(Album $album): int
     {
