caf563450660b51855ec2e455d199275502a82c4
Hidran Arias <hidran@gmail.com>
2022-09-22 18:39:51 +0200

    Aggiungere thumbnail nella creazione dell'album

--- app/Http/Controllers/AlbumsController.php
+++ app/Http/Controllers/AlbumsController.php
@@ -16,6 +16,8 @@ class AlbumsController extends Controller
      */
     public function index(Request $request): View
     {
+
+
         $queryBuilder = Album::orderBy('id', 'DESC');
         if ($request->has('id')) {
             $queryBuilder->where('id', '=', $request->input('id'));
@@ -39,6 +41,10 @@ class AlbumsController extends Controller
 
     /**
      * Store a newly created resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request): RedirectResponse
     {
@@ -49,6 +55,11 @@ class AlbumsController extends Controller
         $album->user_id = 1;
         $album->album_thumb = '/';
         $res = $album->save();
+        if ($request->hasFile('album_thumb')) {
+
+            $this->processFile($request, $album);
+            $res = $album->save();
+        }
         //$res =  Album::create($data);
         $messaggio = $res ? 'Album   ' . $data['album_name'] . ' Created' : 'Album ' . $data['album_name'] . ' was not crerated';
         session()->flash('message', $messaggio);
@@ -58,8 +69,9 @@ class AlbumsController extends Controller
     /**
      * Display the specified resource.
      *
+     * @param \App\Models\Album $album
      *
-     * @return Album $album
+     * @return \App\Models\Album $album
      */
     public function show(Album $album): Album
     {
@@ -68,6 +80,10 @@ class AlbumsController extends Controller
 
     /**
      * Show the form for editing the specified resource.
+     *
+     * @param \App\Models\Album $album
+     *
+     * @return \Illuminate\Http\Response
      */
     public function edit(Album $album): View
     {
@@ -80,6 +96,11 @@ class AlbumsController extends Controller
 
     /**
      * Update the specified resource in storage.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
+     *
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function update(Request $request, Album $album): RedirectResponse
     {
@@ -87,20 +108,22 @@ class AlbumsController extends Controller
         $album->album_name = $data['album_name'];
         $album->description = $data['description'];
         if ($request->hasFile('album_thumb')) {
+
             $this->processFile($request, $album);
         }
-
         $res = $album->save();
-
-
         $messaggio = $res ? 'Album   ' . $album->album_name . ' Updated' : 'Album ' . $album->album_name . ' was not updated';
         session()->flash('message', $messaggio);
-
         return redirect()->route('albums.index');
     }
 
+
     /**
      * Remove the specified resource from storage.
+     *
+     * @param \App\Models\Album $album
+     *
+     * @return int
      */
     public function destroy(Album $album): int
     {
@@ -115,5 +138,6 @@ class AlbumsController extends Controller
     public function delete(Album $album): int
     {
         return $this->destroy($album);
+
     }
 }
