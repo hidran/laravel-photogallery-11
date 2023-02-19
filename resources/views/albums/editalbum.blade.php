ef929747863cc9c1aecbd89d400ffc691170cabe
Hidran Arias <hidran@gmail.com>
2023-02-19 15:27:22 +0100

    Dashboard. Layout navbar e bottoni del form update album

--- resources/views/albums/editalbum.blade.php
+++ resources/views/albums/editalbum.blade.php
@@ -1,33 +1,34 @@
-@extends('templates.default')
-@section('content')
     @php
         /**
-     * * @var $album App\Models\Album
-     */
-    @endphp
-    <h3>EDIT ALBUM {{$album->album_name}}</h3>
+     * @var $album App\Models\Album;
+    */
+@endphp
+@extends('templates.default')
+@section('content')
+    <h1>Edit Album</h1>
     @include('partials.inputerrors')
-    <form method="post" action="{{route('albums.update',['album' => $album->id])}}" enctype="multipart/form-data">
+    <form action="{{route('albums.update',['album' => $album->id])}}" method="POST" enctype="multipart/form-data">
+        @csrf
         @method('PATCH')
         @csrf
         <!-- {{method_field('PATCH')}}
         <input type="hidden" name="_method" value="PATCH">
         -->
-        <div class="form-group  mb-3">
-            <label for="album_name" class="form-label">Name</label>
-            <input class="form-control" name="album_name" id="album_name"
-                   value="{{$album->album_name}}">
-        </div>
-
-        @include('albums.partials.fileupload')
-
         <div class="mb-3">
-            <label for='description' class="form-label">Description</label>
-            <textarea class='form-control' name='description'
-                      id='description'>{{$album->description}}</textarea>
+            <label for="album_name" class="form-label">Name</label>
+            <input class="form-control" name="album_name" id="album_name" value="{{$album->album_name}}">
         </div>
-        <div class="form-group  mb-3">
-            <button class="btn btn-primary">INVIA</button>
+        @include('albums.partials.fileupload')
+        <div class="form-group">
+            <label for="">Description</label>
+            <textarea name="description" id="description" class="form-control"
+                      placeholder="Album description">{{$album->description}}</textarea>
+
+        </div>
+        <div class="d-flex justify-content-end border">
+            <button type="submit" class="btn btn-primary mx-1">Submit</button>
+            <a href="{{route('albums.index')}}" class="btn btn-outline-info  mx-1">Back</a>
+            <a href="{{route('albums.images', $album->id)}}" class="btn btn-outline-success  mx-1">Images</a>
         </div>
     </form>
-@endsection
+@stop
