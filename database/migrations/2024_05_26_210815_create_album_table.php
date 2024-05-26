f5f89341d7dd64e4d4e02c67d0fe47bbcb1f66ec
Hidran Arias <hidran@gmail.com>
2024-05-26 23:24:49 +0200

    Adds albums and phots migrations

---
+++ database/migrations/2024_05_26_210815_create_album_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('album_name',128)->unique()->nullable(false);
+            $table->string('album_thumb');
+            $table->text('description')->nullable();
+            $table->foreignId('user_id');
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('albums');
+    }
+};
