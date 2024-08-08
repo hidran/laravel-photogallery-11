85bd9c4c790aff7da38195741a61bed84de010fc
Hidran Arias <hidran@gmail.com>
2024-08-08 22:40:07 +0200

    Update web.php
    add controller import

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AlbumsController;
+use App\Http\Controllers\PhotosController;
 use App\Http\Controllers\ProfileController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
