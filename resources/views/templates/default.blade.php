618b06342829521fe3f21dbf16426a3cfc2bd7c3
Hidran Arias <hidran@gmail.com>
2024-08-09 10:52:34 +0200

    Adds lightbox to the gallery

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -18,7 +18,7 @@
         crossorigin="anonymous">
     <link rel="stylesheet"
           href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
-
+    <link rel="stylesheet" href="/css/lightbox.css">
     <style>
         body {
             padding: 70px 15px 0;
@@ -114,6 +114,7 @@
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
         crossorigin="anonymous"></script>
+    <script src="/js/lightbox.min.js"></script>
 @show
 </body>
 </html>
