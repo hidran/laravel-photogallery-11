baf002e2bb9a923d65feaae64d36b2aaeb7cb7b3
Hidran Arias <hidran@gmail.com>
2024-08-09 16:38:12 +0200

    Adds scope to categories to filter them by user

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,8 +2,15 @@
 
 namespace App\Models;
 
+use Database\Factories\CategoryFactory;
+use Eloquent;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Carbon;
 
 /**
  * App\Models\Category
@@ -12,21 +19,21 @@ use Illuminate\Database\Eloquent\Model;
  * @property string $category_name
  * @property int $user_id
  * @property string|null $deleted_at
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Album[] $albums
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read Collection|Album[] $albums
  * @property-read int|null $albums_count
- * @method static \Database\Factories\CategoryFactory factory(...$parameters)
- * @method static \Illuminate\Database\Eloquent\Builder|Category newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Category newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Category query()
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereCategoryName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Category whereUserId($value)
- * @mixin \Eloquent
+ * @method static CategoryFactory factory(...$parameters)
+ * @method static Builder|Category newModelQuery()
+ * @method static Builder|Category newQuery()
+ * @method static Builder|Category query()
+ * @method static Builder|Category whereCategoryName($value)
+ * @method static Builder|Category whereCreatedAt($value)
+ * @method static Builder|Category whereDeletedAt($value)
+ * @method static Builder|Category whereId($value)
+ * @method static Builder|Category whereUpdatedAt($value)
+ * @method static Builder|Category whereUserId($value)
+ * @mixin Eloquent
  */
 class Category extends Model
 {
@@ -38,11 +45,16 @@ class Category extends Model
     {
         return $this->belongsToMany(Album::class)->withTimestamps();
     }
-    public function user()
+
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
-    public function scopeGetCategoriesByUserId(Builder $builder, User $user){
+
+    public function scopeGetCategoriesByUserId(
+        Builder $builder,
+        User $user
+    ): void {
         $builder->whereUserId($user->id)->withCount('albums')->orderBy('category_name');
     }
 
