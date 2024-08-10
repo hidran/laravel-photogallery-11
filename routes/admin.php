9a12cdb1269070eb1a4a547c2c54cb2ab0dd01fe
Hidran Arias <hidran@gmail.com>
2024-08-11 00:04:58 +0200

    Adds restore record action

--- routes/admin.php
+++ routes/admin.php
@@ -9,7 +9,8 @@ Route::prefix('admin')->middleware([
 ])->group(function () {
     Route::get('getUsers',
         [AdminUsersController::class, 'getUsers'])->name('admin.getUsers');
-
+    Route::patch('restore/{user}',
+        [AdminUsersController::class, 'restore'])->name('admin.userrestore');
     Route::resource('users', AdminUsersController::class);
     Route::view('/', 'templates/admin')->name('admin');
     Route::get('/dashboard', static function () {
